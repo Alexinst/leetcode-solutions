4e4ae789d1fecc570c9fa2c9fef8a89347f36c87
alex <alexinst00@gmail.com>
2019-05-14 11:35:53 +0800

    P0033 completed.

--- C&C++/0033_search_in_rotated_array.cpp
+++ C&C++/0033_search_in_rotated_array.cpp
@@ -1,57 +1,120 @@
+/*
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
 
+你可以假设数组中不存在重复的元素。
+你的算法时间复杂度必须是 O(log n) 级别。
+
+示例 1:
+    输入: nums = [4,5,6,7,0,1,2], target = 0
+    输出: 4
+
+示例 2:
+    输入: nums = [4,5,6,7,0,1,2], target = 3
+    输出: -1
+*/
 
 #include <iostream>
 #include <vector>
 
 using std::vector;
 
-class Solution {
+class MySolution {
 public:
-    int search(vector<int>& nums, int target) {
-        if (nums.size() < 1)
-            return -1;
+	int search(vector<int>& nums, int target)
+	{
+		if (nums.size() == 0)
+			return -1;
+		
+		int left = 0, right = nums.size() - 1;
+		int index = -1;
+		bool isRotated = false;
+		if (nums[left] > nums[right])
+			isRotated = true;
 
-        int left = 0, right = nums.size() - 1;
-        
-        while(true)
-        {
-            int middle = left + (right - left) / 2;
-            if (target == nums[middle])
-                return middle;
-            
-            if (left == right)
-                return -1;
-            
-            if (target < nums[middle])             // 寻找值小于中间值
-            {
-                if (nums[left] < nums[middle])      // 旋转点不在左边
-                {
-                    if (nums[left] < target)
-                        right = middle - 1;
-                    else if (nums[left] > target)
-                        left = middle + 1;
-                    else
-                        return left;
-                }
-                else                                // 旋转点在左边
-                    right = middle - 1;
-            }
-            
-            else                                        // 中间值小于寻找值
-            {
-                if (nums[middle] < nums[right])         // 旋转点不在右边
-                {
-                    if (target < nums[right])
-                        left = middle + 1;
-                    else if (nums[right] < target)
-                        right = middle - 1;
-                    else
-                        return right;
-                }
-                else                                    // 旋转点在右边
-                    left = middle + 1;
-            }
-        }
-    }
+		if (!isRotated)
+		{
+			if (nums[left] <= target && target <= nums[right])
+				index = binarySearch(nums, left, right, target);
+			else
+				return -1;
+		}
+
+		else
+		{	
+			// rotated: 旋转点左段的最大值的索引
+			int rotated = findRotated(nums);
+			if (nums[left] <= target && target <= nums[rotated])
+				index = binarySearch(nums, left, rotated, target);
+			else if (nums[rotated + 1] <= target && target <= nums[right])
+				index = binarySearch(nums, rotated + 1, right, target);
+			else
+				return -1;
+		}
+		return index;
+	}
+
+	int binarySearch(vector<int>& nums, int left, int right, int target)
+	{
+		while (left <= right)
+		{
+			if (target < nums[left] || target > nums[right])
+				return -1;
+
+			int middle = left + (right - left) / 2;
+			if (target == nums[middle])
+				return middle;
+			else if (target < nums[middle])
+				right = middle - 1;
+			else
+				left = middle + 1;
+		}
+		return -1;
+	}
+
+	int findRotated(vector<int>& nums)
+	{
+
+		int left = 0, right = nums.size() - 1;
+		while (true)
+		{
+			int middle = left + (right - left) / 2;
+			if (left == middle)
+				return left;
+
+			if (nums[left] <= nums[middle])
+				left = middle;
+			else
+				right = middle;
+		}
+	}
 };
 
+class Solution1 {
+public:
+	int search(vector<int>& nums, int target) {
+		int len = nums.size();
+		int lo = 0;
+		int hi = len - 1;
+		while (lo <= hi) {
+			int mid = lo + ((hi - lo) >> 1);
+			if (nums[mid] == target)
+				return mid;
+			if (nums[lo] <= nums[mid]) {
+				if (nums[lo] <= target && target <= nums[mid])
+					hi = mid - 1;
+				else
+					lo = mid + 1;
+			}
+			else {
+				if (nums[mid] <= target && target <= nums[hi])
+					lo = mid + 1;
+				else
+					hi = mid - 1;
+			}
+		}
+
+		return -1;
+	}
+};
