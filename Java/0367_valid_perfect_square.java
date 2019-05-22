cdefd6bb357ba50599ba96736e250cd21bb369bd
alex <alexinst00@gmail.com>
2019-05-22 20:02:43 +0800

    Complete java version of P0367

--- Java/0367_valid_perfect_square.java
+++ Java/0367_valid_perfect_square.java
@@ -1,19 +1,43 @@
-class Solution {
+/*
+给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+
+说明：不要使用任何内置的库函数，如  sqrt。
+
+示例 1：
+	输入：16
+	输出：True
+
+示例 2：
+	输入：14
+	输出：False
+*/
+
+class MySolution {
     public boolean isPerfectSquare(int num) {
         if (num == 0 || num == 1)
             return true;
-        
+
         int left = 1, right = num / 2;
         while (left <= right)
         {
-            int mid = left + (right - left) / 2;
-            if (num / mid == mid)
+            double mid = left + (right - left) / 2;
+            if ((int)num / mid == mid)
                 return true;
-            else if (num / mid < mid)
-                left = mid + 1;
+            else if (num / mid > mid)
+                left = (int)mid + 1;
             else
-                right = mid - 1;
+                right = (int)mid - 1;
         }
         return false;
+    }
+}
+
+class Solution1 {
+    public boolean isPerfectSquare(int num) {
+        long a = num;
+        while (a * a > num) {
+            a = (a + num / a) >> 1;
+        }
+        return a * a == num;
     }
 }
