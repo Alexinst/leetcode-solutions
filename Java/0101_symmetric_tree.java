5923f50d14df91abba1d86654425954e04b82af9
alex <alexinst00@gmail.com>
2019-05-29 21:15:40 +0800

    Complete java version of #0101

---
+++ Java/0101_symmetric_tree.java
@@ -0,0 +1,60 @@
+/*
+给定一个二叉树，检查它是否是镜像对称的。
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+	    1
+	   / \
+	  2   2
+	 / \ / \
+	3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+	    1
+	   / \
+	  2   2
+	   \   \
+	   3    3
+
+说明:
+	如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+*/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution1 {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
+
+class Solution2 {
+	public boolean isSymmetric(TreeNode root) {
+    	return isMirror(root, root);
+	}
+
+	public boolean isMirror(TreeNode t1, TreeNode t2) {
+    	if (t1 == null && t2 == null) return true;
+    	if (t1 == null || t2 == null) return false;
+    	return (t1.val == t2.val)
+        	&& isMirror(t1.right, t2.left)
+        	&& isMirror(t1.left, t2.right);
+	}
+}
