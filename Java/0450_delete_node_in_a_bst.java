771c8249c8847ba8f74feac875adb364626013dd
alex <alexinst00@gmail.com>
2019-06-06 22:35:42 +0800

    Modify #0450

--- Java/0450_delete_node_in_a_bst.java
+++ Java/0450_delete_node_in_a_bst.java
@@ -1,28 +1,62 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 class Solution {
-    private keyNode = null;
-    
+    private TreeNode keyNode = null;
+    private TreeNode exchange = null;
+
     public TreeNode deleteNode(TreeNode root, int key) {
         search(root, key);
-        
-        if (node.left == null && node.right == null)
-            keyNode = null;
-        else if (node.left == null) {
-            keyNode = keyNode.right.val;
-            keyNode.left = keyNode.right.left;
-            keyNode.right = keyNode.right.right;
+
+        if (keyNode.left == null && keyNode.right == null) {
+            setNull(root, keyNode);
         }
-        else if (node.right == null) {
-            keyNode = keyNode.left.val;
-            keyNode.left = keyNode.left.left;
-            keyNode.right = keyNode.left.right;
+        else if (keyNode.left == null) {
+            replaceAWithB(keyNode, keyNode.right);
+        }
+        else if (keyNode.right == null) {
+            replaceAWithB(keyNode, keyNode.left);
         }
         else {
-            
+            searchMin(keyNode);  // keyNode分支的最小值（最左值）
+            keyNode.val = exchange.val;
+            if (exchange.right == null)
+                setNull(keyNode, exchange);
+            else {
+                exchange.val = exchange.right.val;
+                exchange.left = exchange.right.left;
+                exchange.right = exchange.right.right;
+            }
         }
-        
+
         return root;
     }
-    
+
+    private void replaceAWithB(TreeNode a, TreeNode b) {
+        a.val = b.val;
+        a.left = b.left;
+        a.right = b.right;
+    }
+
+    private void setNull(TreeNode root, TreeNode toBeNull) {
+        if (root.left == toBeNull) {
+            root.left = null;
+            return;
+        }
+        if (root.right == toBeNull){
+            root.right = null;
+            return;
+        }
+        if (root.val <= toBeNull.val)
+            setNull(root.right, toBeNull);
+        else // root.val > toBeNull.val)
+            setNull(root.left, toBeNull);
+    }
+
     private void search(TreeNode node, int val) {
         if (node.val == val)
             keyNode = node;
@@ -30,5 +64,12 @@ class Solution {
             search(node.right, val);
         else
             search(node.left, val);
+    }
+
+    private void searchMin(TreeNode node) {
+        if (node.left != null)
+            searchMin(node.left);
+        else
+            exchange = node;
     }
 }
