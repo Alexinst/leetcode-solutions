3a56f11d322977b5aa6427698b2155d34769b548
alex <alexinst00@gmail.com>
2019-06-07 19:09:39 +0800

    Complete java version of #0450

--- Java/0450_delete_node_in_a_bst.java
+++ Java/0450_delete_node_in_a_bst.java
@@ -5,113 +5,138 @@ class TreeNode {
     TreeNode(int x) { val = x; }
 }
 
-class Solution {
-    private TreeNode keyNode = null;
-    private TreeNode exchange = null;
-
+class Solution1 {
     public TreeNode deleteNode(TreeNode root, int key) {
-        search(root, key);
+        if (root == null) return null;
 
-        if (keyNode.left == null && keyNode.right == null) {
-            setNull(root, keyNode);
-        }
-        else if (keyNode.left == null) {
-            replaceAWithB(keyNode, keyNode.right);
-        }
-        else if (keyNode.right == null) {
-            replaceAWithB(keyNode, keyNode.left);
-        }
-        else {
-            searchMin(keyNode);  // keyNode分支的最小值（最左值）
-            keyNode.val = exchange.val;
-            if (exchange.right == null)
-                setNull(keyNode, exchange);
-            else {
-                exchange.val = exchange.right.val;
-                exchange.left = exchange.right.left;
-                exchange.right = exchange.right.right;
-            }
+        // 通过一个DummyHead，处理头节点的特殊情况
+        TreeNode head = new TreeNode(0);
+        head.left = root;
+
+        TreeNode[] nodes = findNode(root, head,-1, key);
+        if (nodes == null) return root;  // 二叉搜索树中不存在值key
+
+        // nodes[1] 为key对应的节点，nodes[0]为该节点的父节点
+        // 当前节点左子树为空，删除当前节点后，父节点指向当前节点右子树
+        if (nodes[1].left == null){
+            if(nodes[2].val == -1)
+                nodes[0].left = nodes[1].right;
+            else
+                nodes[0].right = nodes[1].right;
         }
 
-        return root;
+        else{
+            //当前节点左子树不为空，删除当前节点后，父节点指向当前节点左子树，左子树最右节点指向当前节点右子树
+            TreeNode rightest = findRightestNode(nodes[1].left);
+            if(nodes[2].val == -1)  //当前节点在父节点的左边
+                nodes[0].left = nodes[1].left;
+            else
+                nodes[0].right = nodes[1].left;
+            rightest.right = nodes[1].right;
+        }
+        nodes[1].left = null;
+        nodes[1].right = null;
+        return head.left;
     }
 
-    private void replaceAWithB(TreeNode a, TreeNode b) {
-        a.val = b.val;
-        a.left = b.left;
-        a.right = b.right;
+    // 获取root及子树中值为key的节点，参数pos为0时表示:root为pre的左子树，为1时表示为右子树
+    private TreeNode[] findNode(TreeNode root, TreeNode pre, int pos, int key){
+        if(root == null) return null;
+
+        if(root.val < key)
+            return findNode(root.right, root, 1,  key);
+        else if(root.val > key)
+            return findNode(root.left, root, -1, key);
+
+        TreeNode[] res = new TreeNode[3];
+        res[0] = pre;
+        res[1] = root;
+        res[2] = new TreeNode(pos);
+        return res;
     }
 
-    private void setNull(TreeNode root, TreeNode toBeNull) {
-        if (root.left == toBeNull) {
-            root.left = null;
-            return;
+    private TreeNode findRightestNode(TreeNode root){
+        if(root == null)
+            return null;
+        TreeNode node = root;
+        while(node.right != null){
+            node = node.right;
         }
-        if (root.right == toBeNull){
-            root.right = null;
-            return;
-        }
-        if (root.val <= toBeNull.val)
-            setNull(root.right, toBeNull);
-        else // root.val > toBeNull.val)
-            setNull(root.left, toBeNull);
-    }
-
-    private void search(TreeNode node, int val) {
-        if (node.val == val)
-            keyNode = node;
-        else if (node.val < val)
-            search(node.right, val);
-        else
-            search(node.left, val);
-    }
-
-    private void searchMin(TreeNode node) {
-        if (node.left != null)
-            searchMin(node.left);
-        else
-            exchange = node;
+        return node;
     }
 }
 
-class Solution {
-    private List<TreeNode> list = new ArrayList<>();
-    private int indexKey = -1;
-    private Stack<TreeNode> stack = new Stack<>();
-    
-    public TreeNode deleteNode(TreeNode root, int key) {
-        sort(root, key);
-        if (indexKey == 0)
-            return root.right;
-        else if (indexKey == list.size() - 1)
-            return root.left;
-        else {
-            TreeNode nodeKey = list.get(indexKey);
-            if (nodeKey.left == null && nodeKey.right == null)
-                setNull(nodeKey);
-            else if (nodeKey.left == null)
-                repAWithB(nodeKey, nodeKey.right);
-            else if (nodeKey.right == null)
-                repAWithB(nodeKey, nodeKey.left);
-            else {
-                
-            }
+class Solution2 {
+    public TreeNode findMin(TreeNode node){
+        while (node.left != null){
+            node = node.left;
         }
+        return node;
     }
-    
-    private void sort(TreeNode root, int key) {
-        
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null){
+            return root;
+        }
+        if (root.val < key){
+            root.right = deleteNode(root.right,key);
+        }else if (root.val > key){
+            root.left = deleteNode(root.left,key);
+        }else{
+            if (root.left == null){
+                return root.right;
+            }else if (root.right == null){
+                return root.left;
+            }
+            TreeNode min = findMin(root.right);
+            root.val = min.val;
+            root.right = deleteNode(root.right,root.val);
+        }
+        return root;
     }
-    
-    private void setNull(TreeNode node)
-    {
-        
+}
+
+class Solution3 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(key<root.val){
+            root.left=deleteNode(root.left,key);
+            return root;
+        }
+        if(key>root.val){
+            root.right=deleteNode(root.right,key);
+            return root;
+        }
+        if(root.left==null){
+            return root.right;
+        }
+        if(root.right==null){
+            return root.left;
+        }
+        //找到右子树上的最小值
+        TreeNode successor=findMin(root.right);
+        //将找到的新根节点代替需要删除的节点
+        successor.right=deleteMin(root.right);
+        successor.left=root.left;
+        return successor;
     }
-    
-    private void repAWithB(TreeNode a, TreeNode b)
-    {
-        a.val = b.val;
-        a.left = b.left;
-        a.right = b.right;
+    //找到二叉搜索树中数值最小的节点
+    private TreeNode findMin(TreeNode node){
+        if(node.left==null||node==null){
+            return node;
+        }
+        return findMin(node.left);
+    }
+    private TreeNode deleteMin(TreeNode node){
+        if(node==null){
+            return null;
+        }
+        if(node.left==null){
+            return node.right;
+        }
+        node.left=deleteMin(node.left);
+        return node;
     }
 }
