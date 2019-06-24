0795c30204e1cfee3c10bb053a5f41da84d9483e
alex <alexinst00@gmail.com>
2019-06-24 16:59:29 +0800

    Complete Java version of #0028

---
+++ Java/0028_implement_strStr.java
@@ -0,0 +1,46 @@
+/*
+实现 strStr() 函数。
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1:
+	输入: haystack = "hello", needle = "ll"
+	输出: 2
+
+示例 2:
+	输入: haystack = "aaaaa", needle = "bba"
+	输出: -1
+
+说明:
+	当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+
+	对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+*/
+
+class MySolution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0) &&
+                haystack.substring(i, i + needle.length()).equals(needle))
+
+                return i;
+        }
+
+        return -1;
+    }
+}
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null ) return -1;
+        if (needle.length() == 0) return 0;
+        int length = needle.length();
+        for (int i = 0; i <= haystack.length() - length; i++) {
+            if (haystack.substring(i, i + length).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
