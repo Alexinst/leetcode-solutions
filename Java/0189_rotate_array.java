36be5afe10a916e999883dcf3c71cb0af334f674
alex <alexinst00@gmail.com>
2019-08-01 23:44:25 +0800

    Solve #0189 in Java

---
+++ Java/0189_rotate_array.java
@@ -0,0 +1,67 @@
+/*
+ * https://leetcode-cn.com/problems/rotate-array/
+
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+	输入: [1,2,3,4,5,6,7] 和 k = 3
+	输出: [5,6,7,1,2,3,4]
+	解释:
+	向右旋转 1 步: [7,1,2,3,4,5,6]
+	向右旋转 2 步: [6,7,1,2,3,4,5]
+	向右旋转 3 步: [5,6,7,1,2,3,4]
+
+示例 2:
+	输入: [-1,-100,3,99] 和 k = 2
+	输出: [3,99,-1,-100]
+	解释: 
+	向右旋转 1 步: [99,-1,-100,3]
+	向右旋转 2 步: [3,99,-1,-100]
+
+说明:
+    1. 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+    2. 要求使用空间复杂度为 O(1) 的 原地 算法。
+
+----------------------------------------------------------------------------
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+	Input: [1,2,3,4,5,6,7] and k = 3
+	Output: [5,6,7,1,2,3,4]
+	Explanation:
+	rotate 1 steps to the right: [7,1,2,3,4,5,6]
+	rotate 2 steps to the right: [6,7,1,2,3,4,5]
+	rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+	Input: [-1,-100,3,99] and k = 2
+	Output: [3,99,-1,-100]
+	Explanation: 
+	rotate 1 steps to the right: [99,-1,-100,3]
+	rotate 2 steps to the right: [3,99,-1,-100]
+
+Note:
+    1. Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+    2. Could you do it in-place with O(1) extra space?
+*/
+
+class Solution1 {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+}
