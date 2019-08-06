d490020af8f88455af8191526e07c6f126eb1d3f
alex <alexinst00@gmail.com>
2019-08-06 15:19:56 +0800

    Solve #0100 in Java

---
+++ Java/0100_same_tree.java
@@ -0,0 +1,92 @@
+/*
+ * https://leetcode-cn.com/problems/same-tree/
+
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+	输入:    1         1
+		  	/ \       / \
+		   2   3     2   3
+
+		  [1,2,3],   [1,2,3]
+
+	输出: true
+
+示例 2:
+	输入:  1         1
+		  /           \
+		 2             2
+
+		[1,2],     [1,null,2]
+
+	输出: false
+
+示例 3:
+	输入:  1         1
+		  / \       / \
+		 2   1     1   2
+
+		[1,2,1],   [1,1,2]
+
+	输出: false
+
+--------------------------------------------------------------------------
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+	Input:     1         1
+		      / \       / \
+		     2   3     2   3
+
+		    [1,2,3],   [1,2,3]
+
+	Output: true
+
+Example 2:
+	Input:     1         1
+		      /           \
+		     2             2
+
+		    [1,2],     [1,null,2]
+
+	Output: false
+
+Example 3:
+	Input:     1         1
+		      / \       / \
+		     2   1     1   2
+
+		    [1,2,1],   [1,1,2]
+
+	Output: false
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MySolution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        
+        return isSameTree(p.left, q.left) && 
+               isSameTree(p.right, q.right);
+    }
+}
+
+
