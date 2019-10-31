7c64d3662c05934c63c61187dc3c7772672a130d
alex <alexinst00@gmail.com>
2019-10-31 18:30:02 +0800

    Modify #0621

--- Java/0621_task_scheduler.java
+++ Java/0621_task_scheduler.java
@@ -76,7 +76,3 @@ class Solution2 {
     }
 }
 
-作者：LeetCode
-链接：https://leetcode-cn.com/problems/task-scheduler/solution/ren-wu-diao-du-qi-by-leetcode/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
