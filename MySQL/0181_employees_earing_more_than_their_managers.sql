bb77a37276a007e88e28f97b4bb4ca350c23e536
alex <alexinst00@gmail.com>
2019-11-01 21:56:30 +0800

    Solve #0181 with MySQL.

---
+++ MySQL/0181_employees_earing_more_than_their_managers.sql
@@ -0,0 +1,66 @@
+/*
+ * https://leetcode-cn.com/problems/employees-earning-more-than-their-managers/
+ *
+ * Reference: https://leetcode-cn.com/problems/employees-earning-more-than-their-managers/solution/chao-guo-jing-li-shou-ru-de-yuan-gong-by-leetcode/
+
+Employee 表包含所有员工，他们的经理也属于员工。每个员工都有一个 Id，此外还有一列对应员工的经理的 Id。
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+
+给定 Employee 表，编写一个 SQL 查询，该查询可以获取收入超过他们经理的员工的姓名。在上面的表格中，Joe 是唯一一个收入超过他的经理的员工。
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+
+----------------------------------------------------------------------------------------------------
+
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+*/
+
+
+-- Mine:
+SELECT
+	e1.NAME AS Employee 
+FROM
+	Employee AS e1,
+	Employee AS e2 
+WHERE
+	e1.ManagerId = e2.Id 
+	AND e1.Salary > e2.salary;
+
+
+-- Reference1: 
+SELECT
+	a.NAME AS Employee 
+FROM
+	Employee a
+	INNER JOIN ( SELECT DISTINCT Id, Salary FROM Employee ) b ON a.ManagerId = b.Id 
+WHERE
+	a.Salary > b.Salary;
