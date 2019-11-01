8b9994150be12dbc9c0710a2d37982597f46c9b0
alex <alexinst00@gmail.com>
2019-11-01 21:46:28 +0800

    Solve #0176 with MySQL.

---
+++ MySQL/0176_second_highest_salary.sql
@@ -0,0 +1,60 @@
+/*
+ * https://leetcode-cn.com/problems/second-highest-salary/
+ *
+ * Reference: https://leetcode-cn.com/problems/second-highest-salary/solution/di-er-gao-de-xin-shui-by-leetcode/
+
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+----------------------------------------------------------------------------------------------------
+
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*/
+
+
+-- Mine: 
+SELECT
+	IFNULL( (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1 ), NULL) AS SecondHighSalary;
+
+
+-- Reference1: 
+SELECT
+	Max( Salary ) AS SecondHighestSalary 
+FROM
+	Employee 
+WHERE
+	Salary < (SELECT
+		          Max( Salary ) 
+            FROM
+	            Employee);
