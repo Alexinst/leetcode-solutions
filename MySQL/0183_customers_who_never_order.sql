0b3c4d982e0ffff14287fc62fb2a2f306f75fb07
alex <alexinst00@gmail.com>
2019-11-03 18:07:36 +0800

    Solve #0183 with MySQL.

---
+++ MySQL/0183_customers_who_never_order.sql
@@ -0,0 +1,72 @@
+/*
+ * https://leetcode-cn.com/problems/customers-who-never-order/
+
+某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+
+Customers 表：
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+
+Orders 表：
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+
+例如给定上述表格，你的查询应返回：
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+----------------------------------------------------------------------------------------------------
+
+
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+
+Table: Orders.
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+
+Using the above tables as example, return the following:
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
+
+
+-- My Solution:
+SELECT 
+	name AS Customers 
+FROM
+	Customers 
+WHERE
+	Id NOT IN (SELECT CustomerId FROM Orders);
