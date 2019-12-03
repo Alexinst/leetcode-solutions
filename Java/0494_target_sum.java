5c9bf3af4afeebf10b99ff27857ddcbed3e893dc
Alex <alexinst00@gmail.com>
2019-12-03 22:33:39 +0800

    Update #0494 with Java.

--- Java/0494_target_sum.java
+++ Java/0494_target_sum.java
@@ -1,7 +1,7 @@
 /*
  * https://leetcode-cn.com/problems/target-sum/
  *
- * Reference:
+ * Reference: https://leetcode-cn.com/problems/target-sum/solution/mu-biao-he-by-leetcode/
  
 给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或 -中选择一个符号添加在前面。
 
